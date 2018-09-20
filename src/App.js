b60cfddd4cf7ca047f1adc33f4abec815bf4d54d
“Roni” <roniemail1@gmail.com>
2018-09-20 17:59:06 +0100

    take out babel class plugin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="grid">
+        <div><span> News </span> <span>News </span></div>
+        <div class="media">
+          <h2>Flexbox</h2>
+          <div class="wrapper flex ">
+            <div><span> <img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+             New Range of  coming  soon Style </span>
+            </div>
+
+            <div ><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New in p </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+            <div><img src="http://fakeimg.pl/240x240/666/?text=7"></img>
+            <p> New Style </p> </div>
+          </div>
+        </div>
       </div>
     );
   }
